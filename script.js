1813d2b257f6841a26f4e437e1d9fc27387db408
Pratik <erpratikrajverma@gmail.com>
2024-10-23 22:39:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// let array = [1,2,'a','pratik',4.5]
+ 
+
+// for(let i=0; i<array.length; i++)
+// {
+//     console.log(array[i])
+// }
+
+// for(let i of array){
+//     console.log(i)
+// }
+
+// console.log(array)
+
+
+// console.log(array[3])
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// arr.pop()
+// console.log(arr)
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(arr.includes(10))
+
+// let arr = [
+//     {x:1,y:2},
+//     {x:3,y:4},
+//     {x:5,y:6}
+// ]
+
+// // console.log(arr.includes( {x:1,y:2} ))
+
+// arr.forEach(
+//     (element)=>{
+//         if(element.x === 100 && element.y === 2){
+//             console.log(true)
+//         }
+//         else{
+//             console.log(false)
+//         }
+//     }
+// );
+
+
+
+
+
+// console.log(arr)
+
+ 
